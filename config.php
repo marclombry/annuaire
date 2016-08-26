2b380650d50cba08a268b07cc14286122bd6be72
marclombry <marc.swan.l@gmail.com>
2016-08-27 00:03:39 +0200

    Mise à jour du texte.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 $mysqli = new Mysqli('localhost', 'root', '', 'repertoire');
 if ($mysqli->connect_error) {
-    die('Un probleme est survenue lors de la tentative de connexion a la BDD' . $mysqli->connect_error);
+    die('Erreur de connexion à la base de données' . $mysqli->connect_error);
 }
